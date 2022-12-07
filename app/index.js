f99102fd8ba4d98fa1173ef92e5d680dfd280303
Patryk Wisniewicz <wisniewicz.patryk@gmail.com>
2022-12-07 21:32:42 +0100

    add table data with users of company

--- app/index.js
+++ app/index.js
@@ -16,16 +16,27 @@ async function fetchUsersAndCompanies() {
     
     companiesArray.forEach(company => {
       let numberOfUsers = 0;
+      let userCompany = [];
       usersArray.find(user => {
         if(user.uris.company === company.uri) {
           numberOfUsers++;
+
+          userCompany.push({
+            'userName': user.name,
+            'userEmail': user.email
+          })
         }
       });
+
       company.numberOfUsers = numberOfUsers;
+      company.users = userCompany     
     });
+    
 
     const sortedCompaniesArray = companiesArray.sort((a, b) => {
-      return a.numberOfUsers - b.numberOfUsers;
+      // return a.numberOfUsers - b.numberOfUsers;
+      return b.numberOfUsers - a.numberOfUsers;
+
     })
 
     createTable(sortedCompaniesArray)
@@ -36,12 +47,20 @@ async function fetchUsersAndCompanies() {
 }
 
 const createTable = (data) => {
-  let result = '';
+  let result = '';  
 
 data.forEach(d => {
+  let usersData = '';
+
+  for (let i=0; i < d.users.length; i++) {
+    usersData += `<li>${d.users[i].userName} (${d.users[i].userEmail})</li>`
+  }
+
    result += `<tr>
    <td>${d.name}</td>
    <td>${d.numberOfUsers}</td>
+   <td><ul>${usersData}</ul></td>
+
    </tr>`
  })
  tbody.innerHTML = result
